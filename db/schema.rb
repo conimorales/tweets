1f102c9ad382c198a792e3077082d6b6e86406b3
coni <constanza.morales.a@usach.cl>
2020-06-30 23:28:21 -0400

    add changes database.yml

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_06_30_054322) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tweets", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
