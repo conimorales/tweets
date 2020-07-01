2cce0b055082ffdaacb325cf654f7615d939d41c
coni <constanza.morales.a@usach.cl>
2020-06-30 22:35:36 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'pages/index'
   get 'pages/about'
   get 'pages/example'
-  root 'pages#index'
+  root 'tweets#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
